690b1bf96c1c6961251e7170c4826cdc2c654b20
Aleksander Pininski <aleksander.pininski@gmail.com>
2026-01-28 22:24:11 +0100

    Ostatni

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -2,31 +2,19 @@ provider "azurerm" {
   features {}
 }
 
-# Krok 1: Używamy "data" zamiast "resource", aby odwołać się do ISTNIEJĄCEJ grupy zasobów.
-# To rozwiązuje błąd "already exists". Terraform nie będzie już próbował jej tworzyć.
+# Odczytujemy dane o ISTNIEJĄCEJ grupie zasobów.
 data "azurerm_resource_group" "tfstate" {
   name = var.tfstate_rg_name
 }
 
-# Krok 2: Zasób Storage Account pozostaje jako "resource", ponieważ chcemy nim zarządzać.
-# Odwołuje się on do nazwy i lokalizacji z pobranych danych o grupie zasobów.
-resource "azurerm_storage_account" "tfstate" {
-  name                     = var.storage_account_name
-  resource_group_name      = data.azurerm_resource_group.tfstate.name
-  location                 = data.azurerm_resource_group.tfstate.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  allow_nested_items_to_be_public = false
-  min_tls_version                 = "TLS1_2"
-
-  tags = var.tags
+# Odczytujemy dane o ISTNIEJĄCYM koncie storage.
+data "azurerm_storage_account" "tfstate" {
+  name                = var.storage_account_name
+  resource_group_name = data.azurerm_resource_group.tfstate.name
 }
 
-# Krok 3: Kontener również pozostaje jako "resource" i zawiera poprawkę na deprecated argument.
-resource "azurerm_storage_container" "tfstate" {
-  name                  = var.container_name
-  # Poprawka: Używamy "storage_account_id" zamiast przestarzałego "storage_account_name".
-  storage_account_id    = azurerm_storage_account.tfstate.id
-  container_access_type = "private"
+# Odczytujemy dane o ISTNIEJĄCYM kontenerze.
+data "azurerm_storage_container" "tfstate" {
+  name                 = var.container_name
+  storage_account_name = data.azurerm_storage_account.tfstate.name
 }
--- infra/bootstrap/outputs.tf
+++ infra/bootstrap/outputs.tf
@@ -1,11 +1,14 @@
 output "tfstate_rg_name" {
-  value = data.azurerm_resource_group.tfstate.name
+  description = "The name of the resource group for the remote state."
+  value       = var.tfstate_rg_name
 }
 
 output "storage_account_name" {
-  value = azurerm_storage_account.tfstate.name
+  description = "The name of the storage account for the remote state."
+  value       = var.storage_account_name
 }
 
 output "container_name" {
-  value = azurerm_storage_container.tfstate.name
+  description = "The name of the container for the remote state."
+  value       = var.container_name
 }
