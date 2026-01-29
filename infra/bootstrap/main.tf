6158683f15383870c7ed1e6d662cc628f3a6a0a1
Aleksander Pininski <aleksander.pininski@gmail.com>
2026-01-29 20:33:36 +0100

    FINISH HIM 2

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -5,13 +5,19 @@ provider "azurerm" {
 }
 
 
-data "azurerm_resource_group" "tfstate" {
-  name = var.tfstate_rg_name
+resource "azurerm_resource_group" "tfstate" {
+  name     = var.tfstate_rg_name
+  location = var.location
+  tags     = var.tags
 }
 
-data "azurerm_storage_account" "tfstate" {  # Istniejący SA
-  name                = var.storage_account_name
-  resource_group_name = data.azurerm_resource_group.tfstate.name
+resource "azurerm_storage_account" "tfstate" {  # Tworzymy SA
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS" # Geo-Redundant Storage for resilience
+  tags                     = var.tags
 }
 
 resource "azurerm_storage_container" "tfstate" {  # Nowy kontener
