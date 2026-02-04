a13574ef0fb739915d583139c2be6f6c444780b6
Aleksander Pininski <aleksander.pininski@gmail.com>
2026-02-04 19:45:40 +0100

    X

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -4,11 +4,15 @@ provider "azurerm" {
   subscription_id = var.subscription_id 
 }
 
-
 resource "azurerm_resource_group" "tfstate" {
   name     = var.tfstate_rg_name
   location = var.location
   tags     = var.tags
+
+  # WAŻNE: To zapobiegnie błędom gdy RG już istnieje
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "azurerm_storage_account" "tfstate" {
@@ -16,12 +20,17 @@ resource "azurerm_storage_account" "tfstate" {
   resource_group_name      = azurerm_resource_group.tfstate.name
   location                 = azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
-  account_replication_type = "GRS" # Geo-Redundant Storage for resilience
+  account_replication_type = "GRS"
   tags                     = var.tags
+
+  lifecycle {
+    # Nie próbuj usuwać storage account z tfstate
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_storage_container" "tfstate_container" {
-  name                 = var.container_name
-  storage_account_id   = azurerm_storage_account.tfstate.id  # używamy id zamiast name
+  name                  = var.container_name
+  storage_account_id    = azurerm_storage_account.tfstate.id
   container_access_type = "private"
 }
